cf2ac58a7acda46961b5966f1f2694c097b55657
katf101 <vawav11@gmail.com>
2023-01-01 01:57:38 +0900

    feat:유효성 검사 추가

--- backend/src/routes/authRoutes.ts
+++ backend/src/routes/authRoutes.ts
@@ -1,12 +1,30 @@
 import express, { Router } from "express";
 import authControllers from "../controllers/auth/authControllers";
+import Joi from "joi";
 
-// const authControllers = require("../controllers/auth/authControllers");
-
+const validator = require("express-joi-validation").createValidator({});
 const router: Router = express.Router();
 
-router.post("/register", authControllers.controllers.postRegister);
+const registerSchema = Joi.object({
+  username: Joi.string().min(3).max(12),
+  password: Joi.string().min(6).max(12),
+  mail: Joi.string().email(),
+});
 
-router.post("/login", authControllers.controllers.postLogin);
+const loginSchema = Joi.object({
+  password: Joi.string().min(6).max(12),
+  mail: Joi.string().email(),
+});
+
+router.post(
+  "/register",
+  validator.body(registerSchema),
+  authControllers.controllers.postRegister
+);
+router.post(
+  "/login",
+  validator.body(loginSchema),
+  authControllers.controllers.postLogin
+);
 
 module.exports = router;
