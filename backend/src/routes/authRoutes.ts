71da2198bf7693a698e4a1b4582b54ba8cb36971
hansol <vawav11@gmail.com>
2023-01-02 10:35:33 +0900

    feat: 유효성 검사에 required 옵션 추가

--- backend/src/routes/authRoutes.ts
+++ backend/src/routes/authRoutes.ts
@@ -16,14 +16,14 @@ const router: Router = express.Router();
 const validator = createValidator();
 
 const registerSchema = Joi.object({
-  username: Joi.string().min(3).max(12),
-  password: Joi.string().min(6).max(12),
-  mail: Joi.string().email(),
+  username: Joi.string().min(3).max(12).required(),
+  password: Joi.string().min(6).max(12).required(),
+  mail: Joi.string().email().required(),
 });
 
 const loginSchema = Joi.object({
-  password: Joi.string().min(6).max(12),
-  mail: Joi.string().email(),
+  password: Joi.string().min(6).max(12).required(),
+  mail: Joi.string().email().required(),
 });
 
 router.post(
