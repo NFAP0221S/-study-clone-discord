b9663d6d025fda995525346037b5a28e82ee5ade
hansol <vawav11@gmail.com>
2023-01-02 18:59:09 +0900

    feat: Routing 추가

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,31 @@
 import React from "react";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import "./App.css";
+import LoginPage from "./pages/auth/LoginPage";
+import RegisterPage from "./pages/auth/RegisterPage";
+import Dashboard from "./pages";
 
 function App() {
-  return <div className="App">react react</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        {/* Login */}
+        <Route path="/login">
+          <LoginPage />
+        </Route>
+        {/* register */}
+        <Route path="/register">
+          <RegisterPage />
+        </Route>
+        {/* Dashboard */}
+        <Route path="/dashboard">
+          <Dashboard />
+        </Route>
+        {/* Redirect */}
+        <Route path="/" element={<Navigate replace to="/dashboard" />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
