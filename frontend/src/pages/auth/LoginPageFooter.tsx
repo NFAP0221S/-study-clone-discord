d913f0f33189a1c6a003c3be0e67bd6abd90dec8
hansol <vawav11@gmail.com>
2023-01-12 19:07:44 +0900

    feat: register redirectText

--- frontend/src/pages/auth/LoginPageFooter.tsx
+++ frontend/src/pages/auth/LoginPageFooter.tsx
@@ -1,17 +1,34 @@
 import React from "react";
 import CustomPrimaryButton from "../../shared/components/CustomPrimaryButton";
+import RedirectInfo from "../../shared/components/RedirectInfo";
+import { useNavigate } from "react-router-dom";
 
 const LoginPageFooter = (props: { handleLogin: any; isFormValid: any }) => {
   const { handleLogin, isFormValid } = props;
+
+  const navigate = useNavigate();
+
+  const handlePushToRegisterPage = () => {
+    navigate("/register");
+  };
+
   return (
-    <div>
-      <CustomPrimaryButton
-        label="Log-in"
-        additionalStyles={{ marginTop: "30px" }}
-        disabled={!isFormValid}
-        onClick={handleLogin}
+    <>
+      <div>
+        <CustomPrimaryButton
+          label="Log-in"
+          additionalStyles={{ marginTop: "30px" }}
+          disabled={!isFormValid}
+          onClick={handleLogin}
+        />
+      </div>
+      <RedirectInfo
+        text="Need an account?"
+        redirectText="Create an account"
+        additionalStyles={{ marginTop: "5px" }}
+        redirectHandler={handlePushToRegisterPage}
       />
-    </div>
+    </>
   );
 };
 
