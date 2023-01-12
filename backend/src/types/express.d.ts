4c07223d97d1654c5e6b41f03c74a8510e14e2d3
hansol <vawav11@gmail.com>
2023-01-12 16:05:22 +0900

    middleware/auth/ @ts nocheck

--- backend/src/types/express.d.ts
+++ backend/src/types/express.d.ts
@@ -3,8 +3,8 @@ import jwt from "jsonwebtoken";
 declare global {
   namespace Express {
     interface Request {
-      user?: any;
-      // user: string | jwt.JwtPayload;
+      user: any;
+      // user?: string | jwt.JwtPayload;
     }
   }
 }
