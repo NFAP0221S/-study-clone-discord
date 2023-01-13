37271277a443ac034d269b5b133fb42abce0ce7d
hansol <vawav11@gmail.com>
2023-01-13 14:13:02 +0900

    feat: login form 툴팁 생성

--- frontend/src/pages/auth/LoginPageFooter.tsx
+++ frontend/src/pages/auth/LoginPageFooter.tsx
@@ -1,27 +1,45 @@
 import React from "react";
 import CustomPrimaryButton from "../../shared/components/CustomPrimaryButton";
 import RedirectInfo from "../../shared/components/RedirectInfo";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, NavigateFunction } from "react-router-dom";
+import { Tooltip } from "@mui/material";
 
-const LoginPageFooter = (props: { handleLogin: any; isFormValid: any }) => {
-  const { handleLogin, isFormValid } = props;
+interface LoginPageFooterProps {
+  handleLogin: () => void;
+  isFormValid: boolean;
+}
 
-  const navigate = useNavigate();
+const getFormNotValidMessage = (): string => {
+  return "이메일을 올바르게 입력해주세요. 비밀번호는 6 ~ 12의 길이여야 합니다.";
+};
+const getFormValidMessage = (): string => {
+  return "로그인이 가능합니다!";
+};
 
-  const handlePushToRegisterPage = () => {
+const LoginPageFooter = ({
+  handleLogin,
+  isFormValid,
+}: LoginPageFooterProps): JSX.Element => {
+  const navigate: NavigateFunction = useNavigate();
+
+  const handlePushToRegisterPage = (): void => {
     navigate("/register");
   };
 
   return (
     <>
-      <div>
-        <CustomPrimaryButton
-          label="Log-in"
-          additionalStyles={{ marginTop: "30px" }}
-          disabled={!isFormValid}
-          onClick={handleLogin}
-        />
-      </div>
+      <Tooltip
+        title={!isFormValid ? getFormNotValidMessage() : getFormValidMessage()}
+      >
+        <div>
+          <CustomPrimaryButton
+            label="Log-in"
+            additionalStyles={{ marginTop: "30px" }}
+            disabled={!isFormValid}
+            onClick={handleLogin}
+          />
+        </div>
+      </Tooltip>
       <RedirectInfo
         text="Need an account?"
         redirectText="Create an account"
