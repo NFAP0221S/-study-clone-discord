772cc3a84123ecf0ff248a26f0911e6e48ffeda1
hansol <vawav11@gmail.com>
2023-01-13 15:08:43 +0900

    feat: login form util 함수 작성

--- frontend/src/pages/auth/LoginPage.tsx
+++ frontend/src/pages/auth/LoginPage.tsx
@@ -1,15 +1,22 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import AuthBox from "../../shared/components/AuthBox";
 import LoginPageHeader from "./LoginPageHeader";
 import LoginPageInputs from "./LoginPageInputs";
 import LoginPageFooter from "./LoginPageFooter";
+import { validateLoginForm } from "../../shared/utils/validators";
 
 const LoginPage = (): JSX.Element => {
   const [mail, setMail] = useState<string>("");
   const [password, setPassword] = useState<string>("");
   const [isFormValid, setIsFormValid] = useState<boolean>(false);
 
+  useEffect(() => {
+    setIsFormValid(validateLoginForm({ mail, password }));
+  }, [mail, password]);
+
   const handleLogin = () => {
+    console.log(mail);
+    console.log(password);
     console.log("login in");
   };
 
