d3c11d9ad523237814c3d3fa366eefe617cd49c6
hansol <vawav11@gmail.com>
2023-01-13 15:44:44 +0900

    feat: register page input 요소

--- frontend/src/pages/auth/register/RegisterPage.tsx
+++ frontend/src/pages/auth/register/RegisterPage.tsx
@@ -1,13 +1,29 @@
+import React, { useState } from "react";
 import { Typography } from "@mui/material";
-import React from "react";
 import AuthBox from "../../../shared/components/AuthBox";
+import RegisterPageInputs from "./RegisterPageInputs";
 
 const RegisterPage = (): JSX.Element => {
+  const [mail, setMail] = useState("");
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const [isFormValid, setIsFormValid] = useState(false);
+
   return (
     <AuthBox>
       <Typography variant="h5" sx={{ color: "white" }}>
         Create an account
       </Typography>
+
+      <RegisterPageInputs
+        mail={mail}
+        setMail={setMail}
+        username={username}
+        setUsername={setUsername}
+        password={password}
+        setPassword={setPassword}
+      />
     </AuthBox>
   );
 };
---
+++ frontend/src/pages/auth/register/RegisterPageInputs.tsx
@@ -0,0 +1,48 @@
+import React, { Dispatch, SetStateAction } from "react";
+import InputWithLabel from "../../../shared/components/InputWithLabel";
+
+interface RegisterInputsProps {
+  mail: string;
+  setMail: Dispatch<SetStateAction<string>>;
+  username: string;
+  setUsername: Dispatch<SetStateAction<string>>;
+  password: string;
+  setPassword: Dispatch<SetStateAction<string>>;
+}
+
+const RegisterPageInputs = ({
+  mail,
+  setMail,
+  username,
+  setUsername,
+  password,
+  setPassword,
+}: RegisterInputsProps): JSX.Element => {
+  return (
+    <>
+      <InputWithLabel
+        value={mail}
+        setValue={setMail}
+        label="E-mail address"
+        type="text"
+        placeholder={"Enter e-mail address"}
+      />
+      <InputWithLabel
+        value={username}
+        setValue={setUsername}
+        label="Username"
+        type="password"
+        placeholder={"Enter a username"}
+      />
+      <InputWithLabel
+        value={password}
+        setValue={setPassword}
+        label="Password"
+        type="password"
+        placeholder={"Enter password"}
+      />
+    </>
+  );
+};
+
+export default RegisterPageInputs;
