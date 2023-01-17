e7100106657e8e026db883bc1179cbe4b4aa40e9
hansol <vawav11@gmail.com>
2023-01-17 17:17:29 +0900

    feat: 회원가입 시 jwt localStorage 저장 구현

--- frontend/src/pages/auth/register/RegisterPage.tsx
+++ frontend/src/pages/auth/register/RegisterPage.tsx
@@ -1,22 +1,37 @@
 import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
 import { Typography } from "@mui/material";
+import { register } from "../../../store/actions/authThunks";
 import AuthBox from "../../../shared/components/AuthBox";
 import RegisterPageInputs from "./RegisterPageInputs";
 import RegisterPageFooter from "./RegisterPageFooter";
 import { validateRegisterForm } from "../../../shared/utils/validators";
 
 const RegisterPage = (): JSX.Element => {
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
   const [mail, setMail] = useState("");
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
 
   const [isFormValid, setIsFormValid] = useState(false);
 
+  const { registerDone } = useSelector((state: any) => state.user);
+
   const handleRegister = () => {
     console.log(mail);
     console.log(username);
     console.log(password);
     console.log("registering");
+    dispatch(
+      register({
+        // userDetails: { mail: mail, password: password, username: username },
+        email: mail,
+        password: password,
+        username: username,
+      })
+    );
   };
 
   useEffect(() => {
@@ -28,6 +43,12 @@ const RegisterPage = (): JSX.Element => {
       })
     );
   }, [mail, username, password]);
+
+  useEffect(() => {
+    if (registerDone) {
+      navigate("/login");
+    }
+  }, [registerDone, navigate]);
 
   return (
     <AuthBox>
