c6ad1e146d0c5c564c59fe8f5e90f479c1114c82
hansol <vawav11@gmail.com>
2023-01-17 17:15:38 +0900

    feat: redux-toolkit user auth 셋팅

--- frontend/src/store/reducers/user.ts
+++ frontend/src/store/reducers/user.ts
@@ -1,18 +1,60 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { login, register } from "../actions/authThunks";
 
-interface UserState {
-  userDetails: string;
+interface UserAuth {
+  me: null;
+  loginLoading: boolean;
+  loginDone: boolean;
+  loginError: null | unknown;
+  registerLoading: boolean;
+  registerDone: boolean;
+  registerError: null | unknown;
 }
 
-const initialState: UserState = {
-  userDetails: "",
+const initialState: UserAuth = {
+  me: null,
+  loginLoading: false, // 로그인
+  loginDone: false,
+  loginError: null,
+  registerLoading: false, // 회원가입
+  registerDone: false,
+  registerError: null,
 };
 
 const userSlice = createSlice({
   name: "user",
   initialState,
   reducers: {},
-  //   extraReducers: (builder) => builder
+  extraReducers: (builder) =>
+    builder
+      .addCase(login.pending, (state) => {
+        state.loginLoading = true;
+        state.loginDone = false;
+        state.loginError = null;
+      })
+      .addCase(login.fulfilled, (state, action) => {
+        state.loginLoading = false;
+        state.me = action.payload;
+        state.loginDone = true;
+      })
+      .addCase(login.rejected, (state, action) => {
+        state.loginLoading = false;
+        state.loginError = action.payload;
+      })
+      .addCase(register.pending, (state) => {
+        state.registerLoading = true;
+        state.registerDone = false;
+        state.registerError = null;
+      })
+      .addCase(register.fulfilled, (state, action) => {
+        state.registerLoading = false;
+        state.registerDone = true;
+        localStorage.setItem("dc-user", JSON.stringify(action.payload));
+      })
+      .addCase(register.rejected, (state, action) => {
+        state.registerLoading = false;
+        state.registerError = action.payload;
+      }),
 });
 
 // export default userSlice;
