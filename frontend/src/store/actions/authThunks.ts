5533163aa34dc8cf8623be26ef54c8a2c9781f4c
hansol <vawav11@gmail.com>
2023-01-18 14:18:13 +0900

    refactor: axios instance 변수로 할당

--- frontend/src/store/actions/authThunks.ts
+++ frontend/src/store/actions/authThunks.ts
@@ -1,23 +1,19 @@
-import axios, { AxiosRequestConfig } from "axios";
-import { createAction, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
+import { createAsyncThunk } from "@reduxjs/toolkit";
 
-// const apiClient = axios.create({
-//   baseURL: "http://localhost:5002/api",
-//   timeout: 1000,
-// });
-
-axios.defaults.baseURL = "http://localhost:5002/api";
-// axios.defaults.withCredentials = true; // front, backend 간 쿠키공유
+const instance = axios.create({
+  baseURL: "http://localhost:5002/api",
+  timeout: 1000,
+});
 
 export const login: any = createAsyncThunk(
   "auth/login",
   // data => userDetails
   async (data, { rejectWithValue }) => {
     try {
-      const response = await axios.post("/auth/login", data);
+      const response = await instance.post("/auth/login", data);
       return response.data;
     } catch (error: any) {
-      // localStorage.setItem("user", JSON.stringify(data));
       return rejectWithValue(error.response.data);
     }
   }
@@ -28,7 +24,8 @@ export const register: any = createAsyncThunk(
   // data => userDetails
   async (data, { rejectWithValue }) => {
     try {
-      const response = await axios.post("/auth/register", data);
+      const response = await instance.post("/auth/register", data);
+      console.log(data);
       return response.data;
     } catch (error: any) {
       return rejectWithValue(error.response.data);
