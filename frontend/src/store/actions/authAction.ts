af90377bda9710a89b1b1090f9f4f42afcfacfcf
hansol <vawav11@gmail.com>
2023-01-25 12:06:21 +0900

    feat: axios jwt response

--- frontend/src/store/actions/authAction.ts
+++ frontend/src/store/actions/authAction.ts
@@ -1,9 +1,24 @@
 import axios from "axios";
 import { createAsyncThunk } from "@reduxjs/toolkit";
+import { logout } from "../../shared/utils/auth";
 
 const instance = axios.create({
   baseURL: "http://localhost:5002/api",
   timeout: 1000,
+});
+
+// 요청 인터셉터 -> then 또는  catch로 처리되기 전에 응답을 가로챌수 있음
+instance.interceptors.request.use((config) => {
+  const userDetails = localStorage.getItem("user");
+
+  if (userDetails) {
+    const token = JSON.parse(userDetails).token;
+    // config.headers.Authorization = `Bearer ${token}`;
+    config.headers = {
+      Authorization: `Bearer ${token}`,
+    };
+  }
+  return config;
 });
 
 export const login: any = createAsyncThunk(
@@ -32,3 +47,13 @@ export const register: any = createAsyncThunk(
     }
   }
 );
+
+// secure routes
+
+const checkResponseCode = (exception: any) => {
+  const responseCode = exception?.response?.status;
+
+  if (responseCode) {
+    (responseCode === 401 || responseCode === 403) && logout();
+  }
+};
