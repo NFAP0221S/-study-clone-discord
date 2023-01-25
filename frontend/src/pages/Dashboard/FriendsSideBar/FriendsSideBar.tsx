2557bb9aa3a39eb8175c74c902fcc8960d7ce0a8
hansol <vawav11@gmail.com>
2023-01-25 14:42:02 +0900

    feat: 친구추가 버튼

--- frontend/src/pages/Dashboard/FriendsSideBar/FriendsSideBar.tsx
+++ frontend/src/pages/Dashboard/FriendsSideBar/FriendsSideBar.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { styled } from "@mui/system";
+import AddFriendButton from "./AddFriendButton";
 
 const MainContainer = styled("div")({
   width: "224px",
@@ -13,7 +14,11 @@ const MainContainer = styled("div")({
 });
 
 const FriendsSideBar = () => {
-  return <MainContainer></MainContainer>;
+  return (
+    <MainContainer>
+      <AddFriendButton />
+    </MainContainer>
+  );
 };
 
 export default FriendsSideBar;
