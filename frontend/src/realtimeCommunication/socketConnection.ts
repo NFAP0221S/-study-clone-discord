af69f4fdee5c6ebf80373deb7d0a312e7e6fb0ed
hansol <vawav11@gmail.com>
2023-01-26 18:55:25 +0900

    feat: client io에서 server io 로 token 보내기

--- frontend/src/realtimeCommunication/socketConnection.ts
+++ frontend/src/realtimeCommunication/socketConnection.ts
@@ -1,9 +1,22 @@
 import io from "socket.io-client";
 
+interface TokenProps {
+  email: string;
+  token: string;
+  username: string;
+}
+
 let socket: any = null;
 
-export const connectWithSocketServer = () => {
-  socket = io("http://localhost:5002");
+export const connectWithSocketServer = (userDetails: TokenProps): void => {
+  console.log("fe socketServer: ", userDetails);
+  const jwtToken = userDetails.token;
+
+  socket = io("http://localhost:5002", {
+    auth: {
+      token: jwtToken,
+    },
+  });
 
   socket.on("connect", () => {
     console.log("socketClient: succesfully connected with socket.io server");
