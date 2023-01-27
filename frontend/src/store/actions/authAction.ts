faf80256d3b9a886e39ab891bd7cd1a68cb0356e
hansol <vawav11@gmail.com>
2023-01-27 16:59:02 +0900

    refactor: instance 분리

--- frontend/src/store/actions/authAction.ts
+++ frontend/src/store/actions/authAction.ts
@@ -1,32 +1,6 @@
-import axios from "axios";
+import { instance } from "./instance";
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import { logout } from "../../shared/utils/auth";
-
-const instance = axios.create({
-  baseURL: "http://localhost:5002/api",
-  timeout: 1000,
-});
-
-// 요청 인터셉터 -> then 또는  catch로 처리되기 전에 응답을 가로챌수 있음
-instance.interceptors.request.use(
-  (config) => {
-    const userDetails = localStorage.getItem("dc-user");
-
-    if (userDetails) {
-      const token = JSON.parse(userDetails).token;
-      // config.headers.Authorization = `Bearer ${token}`;
-      config.headers = {
-        Authorization: `Bearer ${token}`,
-      };
-    }
-    // header에 토큰 추가
-    return config;
-  },
-  (err) => {
-    // 에러일 시 거부
-    return Promise.reject(err);
-  }
-);
 
 export const login: any = createAsyncThunk(
   "auth/login",
---
+++ frontend/src/store/actions/instance.ts
@@ -0,0 +1,27 @@
+import axios from "axios";
+
+export const instance = axios.create({
+  baseURL: "http://localhost:5002/api",
+  timeout: 1000,
+});
+
+// 요청 인터셉터 -> then 또는  catch로 처리되기 전에 응답을 가로챌수 있음
+instance.interceptors.request.use(
+  (config) => {
+    const userDetails = localStorage.getItem("dc-user");
+
+    if (userDetails) {
+      const token = JSON.parse(userDetails).token;
+      // config.headers.Authorization = `Bearer ${token}`;
+      config.headers = {
+        Authorization: `Bearer ${token}`,
+      };
+    }
+    // header에 토큰 추가
+    return config;
+  },
+  (err) => {
+    // 에러일 시 거부
+    return Promise.reject(err);
+  }
+);
