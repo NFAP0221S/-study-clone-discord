5b43d7580f486f9f4e514b84e2e0f5a0f917654e
hansol <vawav11@gmail.com>
2023-01-27 15:28:55 +0900

    test

--- backend/src/socketHandlers/newConnectionUser.ts
+++ backend/src/socketHandlers/newConnectionUser.ts
@@ -1,6 +1,7 @@
 import { addNewConnectedUser as serverStore } from "../serverStore";
 
 export const newConnectionHandler = async (socket: any, io: any) => {
+  // console.log("newConnectionHandler: ", socket);
   const userDetails = socket.user;
 
   serverStore({
