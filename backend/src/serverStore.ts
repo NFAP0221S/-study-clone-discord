dcdd2da3b6f8b15cf81d4ddaa4a1f135da89a932
hansol <vawav11@gmail.com>
2023-01-27 15:28:50 +0900

    refactor: type any -> string

--- backend/src/serverStore.ts
+++ backend/src/serverStore.ts
@@ -2,8 +2,8 @@
 const connectedUsers = new Map();
 
 interface SocketProps {
-  socketId: any;
-  userId: any;
+  socketId: string;
+  userId: string;
 }
 
 // key, value 형태로 저장(set)
@@ -11,4 +11,12 @@ export const addNewConnectedUser = ({ socketId, userId }: SocketProps) => {
   connectedUsers.set(socketId, { userId });
   console.log("Server Store: 새 유저가 연결됨");
   console.log(connectedUsers);
+};
+
+export const removeConnectedUser = (socketId: string) => {
+  if (connectedUsers.has(socketId)) {
+    connectedUsers.delete(socketId);
+    console.log("Server Store: 새 유저가 연결됨");
+    console.log(connectedUsers);
+  }
 };
